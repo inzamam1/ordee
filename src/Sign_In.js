9c7b29379c22636b18ee39ab4d54fb8cf5e0edef
inzamam1 <inzamamsayyed10@gmail.com>
2020-12-11 22:22:02 +0530

    Added two containers for login.

--- src/Sign_In.js
+++ src/Sign_In.js
@@ -1,12 +1,12 @@
-import "./App.css";
+import "./Sign_In.css";
 import { db, auth } from "./firebase";
 import React, { useState, useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import { Button, Input } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
-import "./App.css";
 import pf from "./pf_circle.png";
+
 
 function getModalStyle() {
   const top = 50;
@@ -110,34 +110,7 @@ function App() {
           </form>
         </div>
       </Modal>
-      <Modal open={signin} onClose={() => setOpenSignin(false)}>
-        <div style={modalStyle} className={classes.paper}>
-          <form className="app__signup">
-            <center>
-              <img src={pf} alt="" height="70" width="70" />
-              <br></br>
-
-              <Input
-                placeholder="email"
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <br></br>
-              <Input
-                placeholder="password"
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <br></br>
-              <Button type="submit" onClick={Login}>
-                Login
-              </Button>
-            </center>
-          </form>
-        </div>
-      </Modal>
+      
 
       {user ? (
         user.email === "a@g.com" ? (
@@ -146,10 +119,22 @@ function App() {
           <Redirect to="/Home_Page" />
         )
       ) : (
-        <div className="test">
-          <h1>heyy</h1>
-          <Button onClick={() => setOpen(true)}>SignUp</Button>
-          <Button onClick={() => setOpenSignin(true)}>Login</Button>
+        <div className="Signin">
+          <div className="Signin__Container1">
+          <img  className="Container1__Image"src={"./ordee2.png"} alt=""  />
+          <Button className="Container1_button"onClick={() => setOpen(true)}>SignUp</Button>
+          </div>
+          <div className="Signin__Container2">
+          <form>
+                  <h5>Email:</h5>
+                  <input value={email} onChange={(e) => setEmail(e.target.value)} type="email"/>
+                  <h5>Password:</h5>
+                  <input value={password} onChange={(e) => setPassword(e.target.value)} type="password"/>
+                  <button type="submit" onClick={Login}  className="login__signInBtn">sign in</button>
+                  <p> </p>
+        
+              </form>
+          </div>
         </div>
       )}
     </div>
@@ -157,3 +142,7 @@ function App() {
 }
 
 export default App;
+
+/*<h1>heyy</h1>
+<Button onClick={() => setOpen(true)}>SignUp</Button>
+<Button onClick={() => setOpenSignin(true)}>Login</Button>*/
